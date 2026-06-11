63f92229d2f5c9c7eb7bdbc06e74f5e838691eb5
rrozmetov992 <rrozmetov992@gmail.com>
2026-06-11 08:18:01 +0300

    Add files via upload

---
+++ work_14.py
@@ -0,0 +1,32 @@
+#Boolean 27
+# x,y=map(float, input().split())
+# t1=(x<0 and y>0) or (x<0 and y<0)
+# print(t)
+#Boolean 28
+# x,y=map(float, input().split())
+# t1=(x>0 and y>0) or (x<0 and y<0)
+# print(t)
+#Boolean 29
+# x,y,x1,y1,x2,y2=map(input().split())
+# a=(x>x1 and y<y1) and (x2>x and y2<y)
+# print(a)
+#Boolean 30
+# a,b,c=map(int, input().split())
+# t=a==b and a==c and b==c
+# print(t)
+#Boolean 31
+# a,b,c=map(int, input().split())
+# t=a==b or a==c or b==c
+# print(t)
+#Boolean 32
+# a,b,c=map(int, input().split())
+# t=a**2+b**2==c**2 or a**2+c**2==b**2 or b**2+c**2==a**2
+# print(t)
+#Boolean 33
+# a,b,c=map(int, input().split())
+# t=a+b>c or a+c>b or b+c>a
+# print(t)
+#Boolean 34
+# x,y=map(int, input().split())
+# t=(x+y)%2==1
+# print(t)
